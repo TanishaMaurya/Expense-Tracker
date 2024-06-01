2a6e00fcc1ee9b483a34288116db04d47bbdf55f
Tanisha Maurya <12tanishamaurya@gmail.com>
2024-06-01 21:22:45 +0530

    first commit

---
+++ client/src/context/GlobalState.js
@@ -0,0 +1,90 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+import axios from "axios";
+
+const initialState = {
+  transactions: [],
+  error: null,
+  loading: true,
+};
+
+// Create context
+export const GlobalContext = createContext(initialState);
+// Provider Component
+export const GlobalProvider = ({ children }) => {
+  // Initial state
+
+
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // Actions
+  const getTransactions = async () => {
+    try {
+      const res = await axios.get("/api/v1/transactions");
+      dispatch({
+        type: "GET_TRANSACTIONS",
+        payload: res.data.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: error.response.data.error,
+      });
+    }
+  };
+
+  const deleteTransaction = async (id) => {
+    try {
+      await axios.delete(`/api/v1/transactions/${id}`);
+      dispatch({
+        type: "DELETE_TRANSACTION",
+        payload: id,
+      });
+    } catch (error) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: error.response.data.error,
+      });
+    }
+  };
+
+  const addTransaction = async (transaction) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.post(
+        "/api/v1/transactions/",
+        transaction,
+        config
+      );
+      dispatch({
+        type: "ADD_TRANSACTION",
+        payload: res.data.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "TRANSACTION_ERROR",
+        payload: error.response.data.error,
+      });
+    }
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
+        getTransactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
